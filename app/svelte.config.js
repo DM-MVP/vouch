66582bdbf0c7be098d84cc3c382dd372fa65fb63
vico <ray7551@gmail.com>
2024-12-30 19:10:05 +0800

    use @sveltejs/vite-plugin-svelte

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,7 +1,18 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
+// export default {
+//   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+//   // for more information about preprocessors
+//   preprocess: vitePreprocess(),
+// }
+
+import adapter from '@zeabur/svelte-adapter'; // <-- 注意这里
+ 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter()
+  }
 }
