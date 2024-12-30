0d06e5131a000c86967cde8eeaa5a4bf51504155
vico <ray7551@gmail.com>
2024-12-30 17:36:22 +0800

    fix telegram callback

--- server/routes/telegram.js
+++ server/routes/telegram.js
@@ -1,10 +1,30 @@
 import { vouchTelegram } from '../lib/index.js'
 
 export async function telegramCallback(req, res) {
-  console.log('telegram callback address: ', req.session.address)
   console.log('telegram callback req: ', req.query)
+  /**
+   * example query:
+   * {
+
+  id: '1623571692',
+
+  first_name: 'xxx',
+
+  username: 'xxx_username',
+
+  photo_url: 'https://t.me/i/userpic/320/Z5zPgpUp4lvJTQxfYob_Av5BRMnOx5AecDhDi5QsuZE.jpg',
+
+  auth_date: '1735550832',
+
+  hash: 'xxxx35154207eccd5xx847425c8a133b6bfxd5b98xxce50bf8ab20e4fdxxxxxx'
+
+}
+   */
   try {
-    await vouchTelegram(req.session.address, req.query.user)
+    if (!req.query.address || !req.query.username) {
+      throw new Error("address or username is required")
+    }
+    await vouchTelegram(req.query.address, req.query.username)
   } catch(err) {
     console.error(err)
     if (err.message === "already vouched") {
