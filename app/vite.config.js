08802babfd2021e171a9cd97fb8a4abf09e6e2e1
vico <ray7551@gmail.com>
2024-12-30 19:34:01 +0800

    restore vite configs

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-// import basicSsl from '@vitejs/plugin-basic-ssl'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
-  plugins: [
-    // basicSsl(),
-    svelte()
-  ],
+  plugins: [basicSsl(), svelte()],
   server: {
-    port: 8080,
+    port: 443,
     strictPort: true,
     host: true,
     cors: true
