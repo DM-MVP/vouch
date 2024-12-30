9a01fe88b72f6feb201188fa858d43cc7df49da8
vico <ray7551@gmail.com>
2024-12-30 17:43:16 +0800

    app dev server must at port 443(to work with telegram auth)

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
-  plugins: [svelte()],
+  plugins: [basicSsl(), svelte()],
   server: {
+    port: 443,
+    strictPort: true,
+    host: true,
     cors: true
   },
-  host: '0.0.0.0'
 })
