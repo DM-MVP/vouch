bb466b30e45e96e844d4690e572cc50fb2f978b4
vico <ray7551@gmail.com>
2024-12-30 19:22:35 +0800

    disable ssl

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+// import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
-  plugins: [basicSsl(), svelte()],
+  plugins: [
+    // basicSsl(),
+    svelte()
+  ],
   server: {
-    port: 443,
+    port: 80,
     strictPort: true,
     host: true,
     cors: true
