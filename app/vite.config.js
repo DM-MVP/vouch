9a03c186e0d5f3f4d0e8b6be0b0d3ffdc4aad79c
vico <ray7551@gmail.com>
2025-01-17 23:43:10 +0800

    disable ssl since telegram related area is hidden

--- app/vite.config.js
+++ app/vite.config.js
@@ -5,11 +5,14 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
-  plugins: [basicSsl(), svelte()],
-  server: {
-    port: 443,
-    strictPort: true,
-    host: true,
-    cors: true
-  },
+  plugins: [
+    // basicSsl(),
+    svelte()
+  ],
+  // server: {
+  //   port: 443,
+  //   strictPort: true,
+  //   host: true,
+  //   cors: true
+  // },
 })
